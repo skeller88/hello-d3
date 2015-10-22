dd98d12dc42529c05cf981ba1630336fe199c6c8
Shane Keller <skeller88@gmail.com>
2015-10-21 23:21:23 -0700

    Histogram for one group

--- public/chart.js
+++ public/chart.js
@@ -1,5 +1,488 @@
-// 446 x 333
-var data = [
+var singleGroupData = {
+    "top_treatments": [
+        {
+            "_id": {
+                "treatment": "Vitamin d"
+            },
+            "count": 50
+        },
+        {
+            "_id": {
+                "treatment": "Exercise"
+            },
+            "count": 45
+        },
+        {
+            "_id": {
+                "treatment": "Magnesium"
+            },
+            "count": 38
+        },
+        {
+            "_id": {
+                "treatment": "Ibuprofen"
+            },
+            "count": 37
+        },
+        {
+            "_id": {
+                "treatment": "gluten free"
+            },
+            "count": 28
+        },
+        {
+            "_id": {
+                "treatment": "Meditation"
+            },
+            "count": 27
+        },
+        {
+            "_id": {
+                "treatment": "Gabapentin"
+            },
+            "count": 26
+        },
+        {
+            "_id": {
+                "treatment": "dairy free"
+            },
+            "count": 24
+        },
+        {
+            "_id": {
+                "treatment": "Yoga"
+            },
+            "count": 23
+        },
+        {
+            "_id": {
+                "treatment": "Fish oil"
+            },
+            "count": 22
+        }
+    ],
+    "n_symptoms": [
+        {
+            "_id": 1,
+            "count": 204
+        },
+        {
+            "_id": 2,
+            "count": 149
+        },
+        {
+            "_id": 3,
+            "count": 50
+        },
+        {
+            "_id": 4,
+            "count": 19
+        },
+        {
+            "_id": 5,
+            "count": 22
+        },
+        {
+            "_id": 6,
+            "count": 16
+        },
+        {
+            "_id": 7,
+            "count": 10
+        },
+        {
+            "_id": 8,
+            "count": 5
+        },
+        {
+            "_id": 9,
+            "count": 4
+        },
+        {
+            "_id": 10,
+            "count": 3
+        },
+        {
+            "_id": 11,
+            "count": 3
+        },
+        {
+            "_id": 13,
+            "count": 1
+        },
+        {
+            "_id": 15,
+            "count": 2
+        },
+        {
+            "_id": 17,
+            "count": 1
+        },
+        {
+            "_id": 18,
+            "count": 1
+        },
+        {
+            "_id": 27,
+            "count": 1
+        },
+        {
+            "_id": 46,
+            "count": 1
+        }
+    ],
+    "top_symptoms": [
+        {
+            "_id": {
+                "symptom": "Fatigue"
+            },
+            "count": 281
+        },
+        {
+            "_id": {
+                "symptom": "Headache"
+            },
+            "count": 122
+        },
+        {
+            "_id": {
+                "symptom": "Anxiety"
+            },
+            "count": 97
+        },
+        {
+            "_id": {
+                "symptom": "Brain fog"
+            },
+            "count": 97
+        },
+        {
+            "_id": {
+                "symptom": "Joint pain"
+            },
+            "count": 90
+        },
+        {
+            "_id": {
+                "symptom": "Pain"
+            },
+            "count": 83
+        },
+        {
+            "_id": {
+                "symptom": "Diarrhea"
+            },
+            "count": 80
+        },
+        {
+            "_id": {
+                "symptom": "Nausea"
+            },
+            "count": 80
+        },
+        {
+            "_id": {
+                "symptom": "Depression"
+            },
+            "count": 78
+        },
+        {
+            "_id": {
+                "symptom": "Dizziness"
+            },
+            "count": 55
+        }
+    ],
+    "n_conditions": [
+        {
+            "_id": 1,
+            "count": 350
+        },
+        {
+            "_id": 2,
+            "count": 75
+        },
+        {
+            "_id": 3,
+            "count": 28
+        },
+        {
+            "_id": 4,
+            "count": 20
+        },
+        {
+            "_id": 5,
+            "count": 7
+        },
+        {
+            "_id": 6,
+            "count": 4
+        },
+        {
+            "_id": 7,
+            "count": 4
+        },
+        {
+            "_id": 8,
+            "count": 1
+        },
+        {
+            "_id": 9,
+            "count": 3
+        }
+    ],
+    "top_conditions": [
+        {
+            "_id": {
+                "condition": "Depression"
+            },
+            "count": 96
+        },
+        {
+            "_id": {
+                "condition": "Fibromyalgia"
+            },
+            "count": 84
+        },
+        {
+            "_id": {
+                "condition": "Anxiety"
+            },
+            "count": 80
+        },
+        {
+            "_id": {
+                "condition": "Migraine"
+            },
+            "count": 53
+        },
+        {
+            "_id": {
+                "condition": "Chronic fatigue syndrome"
+            },
+            "count": 46
+        },
+        {
+            "_id": {
+                "condition": "Ehlers-Danlos syndrome"
+            },
+            "count": 44
+        },
+        {
+            "_id": {
+                "condition": "Irritable bowel syndrome"
+            },
+            "count": 37
+        },
+        {
+            "_id": {
+                "condition": "Crohn's disease"
+            },
+            "count": 34
+        },
+        {
+            "_id": {
+                "condition": "Asthma"
+            },
+            "count": 32
+        },
+        {
+            "_id": {
+                "condition": "Hashimoto's disease"
+            },
+            "count": 27
+        }
+    ],
+    "n_users": 492,
+    "n_treatments": [
+        {
+            "_id": 1,
+            "count": 254
+        },
+        {
+            "_id": 2,
+            "count": 72
+        },
+        {
+            "_id": 3,
+            "count": 37
+        },
+        {
+            "_id": 4,
+            "count": 14
+        },
+        {
+            "_id": 5,
+            "count": 17
+        },
+        {
+            "_id": 6,
+            "count": 12
+        },
+        {
+            "_id": 7,
+            "count": 8
+        },
+        {
+            "_id": 8,
+            "count": 9
+        },
+        {
+            "_id": 9,
+            "count": 9
+        },
+        {
+            "_id": 10,
+            "count": 4
+        },
+        {
+            "_id": 11,
+            "count": 6
+        },
+        {
+            "_id": 12,
+            "count": 2
+        },
+        {
+            "_id": 13,
+            "count": 1
+        },
+        {
+            "_id": 14,
+            "count": 3
+        },
+        {
+            "_id": 15,
+            "count": 3
+        },
+        {
+            "_id": 16,
+            "count": 1
+        },
+        {
+            "_id": 17,
+            "count": 3
+        },
+        {
+            "_id": 18,
+            "count": 2
+        },
+        {
+            "_id": 19,
+            "count": 1
+        },
+        {
+            "_id": 20,
+            "count": 2
+        },
+        {
+            "_id": 21,
+            "count": 1
+        },
+        {
+            "_id": 22,
+            "count": 1
+        },
+        {
+            "_id": 23,
+            "count": 2
+        },
+        {
+            "_id": 25,
+            "count": 3
+        },
+        {
+            "_id": 27,
+            "count": 1
+        },
+        {
+            "_id": 28,
+            "count": 1
+        },
+        {
+            "_id": 29,
+            "count": 1
+        },
+        {
+            "_id": 30,
+            "count": 1
+        },
+        {
+            "_id": 33,
+            "count": 1
+        },
+        {
+            "_id": 34,
+            "count": 1
+        },
+        {
+            "_id": 36,
+            "count": 3
+        },
+        {
+            "_id": 39,
+            "count": 1
+        },
+        {
+            "_id": 40,
+            "count": 1
+        },
+        {
+            "_id": 43,
+            "count": 2
+        },
+        {
+            "_id": 49,
+            "count": 1
+        },
+        {
+            "_id": 52,
+            "count": 1
+        },
+        {
+            "_id": 54,
+            "count": 1
+        },
+        {
+            "_id": 56,
+            "count": 1
+        },
+        {
+            "_id": 66,
+            "count": 1
+        },
+        {
+            "_id": 69,
+            "count": 1
+        },
+        {
+            "_id": 71,
+            "count": 1
+        },
+        {
+            "_id": 75,
+            "count": 1
+        },
+        {
+            "_id": 80,
+            "count": 1
+        },
+        {
+            "_id": 86,
+            "count": 1
+        },
+        {
+            "_id": 88,
+            "count": 1
+        },
+        {
+            "_id": 118,
+            "count": 1
+        }
+    ]
+};
+
+var segmentedData = [
     {
         "top_conditions": [
             {
@@ -266,6 +749,33 @@ var chartTitleMargin = 5;
 var innerWidth = width - outerMargin.left - outerMargin.right;
 var innerHeight = height - outerMargin.top - outerMargin.bottom;
 
+
+/**
+ *
+ * @param rawData
+ * @param category {string} "n_symptoms"
+ * @param binSize
+ */
+function chartSingleGroupCounts(rawData, category, binSize) {
+    var mungedData = mungeData(rawData, category);
+    var binSize = binSize || 2;
+    var chartOptions = {
+        binSize: binSize,
+        chartTypeName: mungedData.chartTypeName
+    };
+
+    var data = mungedData.data;
+    var keys = Object.keys(data);
+
+    // keys of data, "_id"s, are the "number of conditions/symptoms/treatments". The value type is String.
+    var extent = d3.extent(keys);
+    var min = parseInt(extent[0]);
+//    // histograms in R hist() are left-open and right-closed by default.
+    var max = parseInt(extent[1]);
+
+    drawHistogramChart(data, max, min, chartOptions);
+}
+
 /**
  * Chart difference between two groups in counts of symptoms, conditions, or treatments.
  * @param rawData {object} the two groups of data wrapped in an object
@@ -274,21 +784,31 @@ var innerHeight = height - outerMargin.top - outerMargin.bottom;
  * @param comparisonName {string} group to compare against baseline
  * @param binSize {int} size of histogram bin
  */
-function chartCounts(rawData, field, baselineName, comparisonName, binSize) {
+function chartBetweenGroupCounts(rawData, field, baselineName, comparisonName, binSize) {
     console.log('Comparing', comparisonName, 'against', baselineName, 'baseline');
 
-    var mungedData = mungeData(rawData, field, baselineName, comparisonName);
+    var mungedData = mungeSegmentedData(rawData, field, baselineName, comparisonName);
     var diffDataRecord = computeGroupDifferences(mungedData.baselineData, mungedData.comparisonData);
     var binSize = binSize || 2;
     var chartOptions = {
         binSize: binSize,
         chartTypeName: mungedData.chartTypeName
     };
-    var totalData = mungedData.baselineData.concat(mungedData.comparisonData);
 
-    drawHistogramChart(totalData, diffDataRecord, chartOptions);
+    // keys of diffDataRecord, "_id"s, are the "number of conditions/symptoms/treatments". The value type is String.
+    var extent = d3.extent(Object.keys(diffDataRecord));
+    var min = extent[0];
+    // histograms in R hist() are left-open and right-closed by default.
+    var max = extent[1];
+
+    drawHistogramChart(diffDataRecord, max, min, chartOptions);
 }
 
+/**
+ *
+ * @param dataObj - {'top_conditions':[], 'n_conditions':[], ...}
+ * @returns string
+ */
 function getChartType(dataObj) {
     var chartTypes = [
         'n_conditions',
@@ -315,14 +835,47 @@ function getChartName(chartType) {
 }
 
 /**
+ * @param dataObj {object}
+ *  {'top_conditions: [
+ *      {"groupBy": {"sex": "female"},
+ *      "values": [{"_id": 0,  "count": 94}, ...]
+ *  }], ...}
+ *  @param category {string}
+ *  @returns {object} {data: {1: 30, 2: 40}}
+ */
+function mungeData(dataObj, category) {
+    if (!dataObj[category]) {
+        console.error('No data for category', category);
+        return;
+    }
+
+    var chartTypeName = getChartName(category);
+
+    var dataRecord = {};
+
+    dataObj[category].forEach(function(el) {
+        dataRecord[el._id] = el.count;
+    });
+
+    return {
+        data: dataRecord,
+        chartTypeName: chartTypeName
+    };
+}
+
+/**
  *
- * @param dataObj
+ * @param dataObj -
+ *  {'top_conditions: [
+ *      {"groupBy": {"sex": "female"},
+ *      "values": [{"_id": 0,  "count": 94}, ...]
+ *  }], ...}
  * @param field
  * @param baselineName
  * @param comparisonName
  * @returns {{baselineData: (*|values|fixture.values|data.values), comparisonData: (*|values|fixture.values|data.values), chartTypeName: *}}
  */
-function mungeData(dataObj, field, baselineName, comparisonName) {
+function mungeSegmentedData(dataObj, field, baselineName, comparisonName) {
     // look up the proper key in data
     var chartType = getChartType(dataObj);
     // populate chart title and x axis
@@ -377,21 +930,21 @@ function computeGroupDifferences(baselineData, comparisonData) {
     return diffDataRecord;
 }
 
-function drawHistogramChart(data, diffDataRecord, chartOptions) {
+/**
+ *
+ * @param data {object} {1: -30, 2: 20, 3: -100, 4: 0}
+ * @param max
+ * @param min
+ * @param chartOptions
+ */
+function drawHistogramChart(data, max, min, chartOptions) {
     // Chart options
     var binSize = chartOptions.binSize || 2;
     var title = 'Number of ' + chartOptions.chartTypeName;
     var xLabel = 'Number of ' + chartOptions.chartTypeName;
     var yLabel = 'Count difference';
 
-    var extent = d3.extent(data, function(d) {
-        // value of "_id" is the "number of conditions/symptoms/treatments". The value type is String.
-        return parseInt(d._id);
-    });
-    var min = extent[0];
-    // histograms in R hist() are left-open and right-closed by default.
-    var max = extent[1];
-    var numBins = ((max + 1 - min) / binSize);
+    var numBins = Math.ceil((max + 1 - min) / binSize);
 
     // object --> array of binned objects
     // {1: -30, 2: 20, 3: -100, 4: 0} --> [{bin: 0, diff: -30}, {bin: 2, diff: -80}, {bin: 4, diff:0}]
@@ -402,8 +955,8 @@ function drawHistogramChart(data, diffDataRecord, chartOptions) {
         var binDiffCount = 0;
 
         for (var i = currentBin; i < currentBin + binSize; i++) {
-            if (diffDataRecord[i] != undefined) {
-                binDiffCount += diffDataRecord[i];
+            if (data[i] != undefined) {
+                binDiffCount += data[i];
             }
         }
 
@@ -492,10 +1045,13 @@ function drawHistogramChart(data, diffDataRecord, chartOptions) {
         .text(title);
 }
 
-//chartCounts(data[1], 'sex', 'male', 'female');
-//chartCounts(data[3], 'sex', 'male', 'female');
-//chartCounts(data[5], 'sex', 'male', 'female');
+chartSingleGroupCounts(singleGroupData, 'n_conditions');
+chartSingleGroupCounts(singleGroupData, 'n_symptoms');
+chartSingleGroupCounts(singleGroupData, 'n_treatments');
+//chartBetweenGroupCounts(segmentedData[1], 'sex', 'male', 'female');
+//chartBetweenGroupCounts(segmentedData[3], 'sex', 'male', 'female');
+//chartBetweenGroupCounts(segmentedData[5], 'sex', 'male', 'female');
 
-chartCounts(data[1], 'sex', 'female', 'male');
-chartCounts(data[3], 'sex', 'female', 'male');
-chartCounts(data[5], 'sex', 'female', 'male');
+//chartBetweenGroupCounts(data[1], 'sex', 'female', 'male');
+//chartBetweenGroupCounts(data[3], 'sex', 'female', 'male');
+//chartBetweenGroupCounts(data[5], 'sex', 'female', 'male');
